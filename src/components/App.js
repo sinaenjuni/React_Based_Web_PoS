7b71eeb2b7214fcbff4ae97b24358cf1dc5b1068
y2k4388 <y2k4388@gmail.com>
2019-06-13 23:51:46 +0900

    'Initialize_Project'

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppShell from './AppShell';
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <AppShell/>
+            </div>
+        );
+    }
+}
+
+export default App;
